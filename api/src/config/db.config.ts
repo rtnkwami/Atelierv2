9aa13bec6d2b73e854aedf7a794f88fb3568de6b
rtnkwami <nukukwami@outlook.com>
2025-09-28 14:06:22 +0000

    feat(api): add db connection

---
+++ api/src/config/db.config.ts
@@ -0,0 +1,26 @@
+import config from "../env.ts";
+import { Sequelize } from "sequelize";
+import logger from "./logger.config.ts";
+
+/** Use database url in case db is switched to a third party database provider */
+const sequelize = config.db.url
+    ? new Sequelize(config.db.url, { logging: false })
+    : new Sequelize(
+        config.db.name,
+        config.db.user,
+        config.db.password,
+        {
+            host: config.db.host,
+            dialect: config.db.dialect,
+            logging: false
+        }
+    );
+
+export const testDbConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        logger.info("Database connection established successfully")
+    } catch (error) {
+        logger.error(error, "Database connection error")
+    }
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app.ts";
 import config from "./env.ts";
 import logger from "./config/logger.config.ts";
+import { testDbConnection } from "./config/db.config.ts";
 
 app.listen(config.api.port, () => {
     logger.info(`Server listening on http://localhost:${config.api.port}...`);
+    
+    testDbConnection();
 });
