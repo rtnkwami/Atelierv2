6464a7128aa1bf8ed48e4025fe286fbe24a388b2
rtnkwami <nukukwami@outlook.com>
2025-09-28 15:01:20 +0000

    fix(api): fix missing db connection export

--- api/src/config/db.config.ts
+++ api/src/config/db.config.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 import logger from "./logger.config.ts";
 
 /** Use database url in case db is switched to a third party database provider */
-const sequelize = config.db.url
+export const sequelize = config.db.url
     ? new Sequelize(config.db.url, { logging: false })
     : new Sequelize(
         config.db.name,
