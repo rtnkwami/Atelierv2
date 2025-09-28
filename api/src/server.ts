135f658fe711286bd642f5d9698aff0c9791b87c
rtnkwami <nukukwami@outlook.com>
2025-09-28 08:06:39 +0000

    feat(api): add basic logger with pretty output for dev

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app.ts";
 import config from "./env.ts";
+import logger from "./config/logger.config.ts";
 
 app.listen(config.api.port, () => {
-    console.log(`Server listening on http://localhost:${config.api.port}...`);
+    logger.info(`Server listening on http://localhost:${config.api.port}...`);
 });
