ee43eb9b5ec5d2aea19d65fc4bdfcbee9236e9b6
rtnkwami <nukukwami@outlook.com>
2025-10-04 15:13:03 +0000

    feat(api): add http logging

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,20 @@
 import express from 'express';
+import type { Request, Response } from 'express';
+import cors from 'cors';
+import { httpLogger } from '@config/logger.config.ts';
 
 const app = express();
+
+app.use(httpLogger);
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ 
+        message: 'Welcome to Atelier'
+    });
+})
 
 export default app;
--- api/src/config/logger.config.ts
+++ api/src/config/logger.config.ts
@@ -1,4 +1,6 @@
 import pino from "pino";
+import { pinoHttp } from "pino-http";
+import type { Request, Response } from "express";
 
 const logger = pino({
     transport: {
@@ -9,5 +11,33 @@ const logger = pino({
     },
     base: null
 });
+
+export const httpLogger = pinoHttp({
+    transport: {
+        target: 'pino-pretty',
+        options: { colorize: true }
+    },
+    serializers: {
+        req: (req: Request) => {
+            return {
+                id: req.id,
+                method: req.method,
+                url: req.url,
+                query: req.query,
+                params: req.params,
+                headers: {
+                    host: req.headers.host,
+                    userAgent: req.headers["user-agent"],
+                    origin: req.headers.origin
+                },
+            }
+        },
+        res: (res: Response) => {
+            return {
+                statusCode: res.statusCode
+            }
+        },
+    }
+})
 
 export default logger;
