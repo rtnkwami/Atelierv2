402958754fc4bad433090575b8ee313bbedb0474
rtnkwami <nukukwami@outlook.com>
2025-10-16 17:50:14 +0000

    feat(db): replace sequelize with prisma & prisma client

--- api/src/config/db.config.ts
+++ api/src/config/db.config.ts
@@ -1,60 +1,11 @@
 import config from "../env";
-import { Dialect, Sequelize } from "sequelize";
+import { PrismaClient } from "../../prisma-client/client";
 
-/**
- * Purpose: This functions exists to provide a sequelize connection for use in syncing models to
- * a database. It's a factory function for reuse in integration tests between repositories and
- * a test database created using a test container.
- * 
- * @param dbConnectionDetails 
- * @param dbUrl 
- * @returns database client
- */
+export const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: config.db.url
+        }
+    },
+});
 
-export const getSequelizeClient = (
-    dbConnectionDetails: {
-        name: string,
-        user: string,
-        password: string,
-        host: string,
-        dialect: Dialect,
-        port: number,
-},
-    dbUrl?: string
-) => {
-
-    let client: Sequelize;
-
-    if (dbUrl){
-        /** Use database url in case db is switched to a third party database provider */
-        client = new Sequelize(dbUrl, { logging: false })
-    } else {
-        client = new Sequelize(
-           dbConnectionDetails.name,
-           dbConnectionDetails.user,
-           dbConnectionDetails.password,
-           {
-               host: dbConnectionDetails.host,
-               port: dbConnectionDetails.port,
-               dialect: dbConnectionDetails.dialect,
-               logging: false
-           }
-       );
-
-    }
-
-    return client;
-};
-
-
-const dbConnectionDetailsFromEnv = {
-    name: config.db.name,
-    user: config.db.user,
-    password: config.db.password,
-    host: config.db.host,
-    dialect: config.db.dialect,
-    port: config.db.port
-}
-
-
-export const dbClient = getSequelizeClient(dbConnectionDetailsFromEnv);
