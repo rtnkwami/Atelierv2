9d14c3aa7e4d482d715a97c2605b51a2f58869dc
rtnkwami <nukukwami@outlook.com>
2025-10-16 18:14:54 +0000

    feat(api): remove unneeded database connection test

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,21 +1,7 @@
 import app from "./app";
 import config from "./env";
 import logger from "./config/logger.config";
-import { initializeDatabase } from "database/init";
-import { dbClient } from "@config/db.config";
-
-async function startDb() {
-    try {
-        await dbClient.authenticate();
-        initializeDatabase(dbClient);
-        logger.info("Database connection established");
-    } catch (error) {
-        logger.error(error, "Database error");
-    }
-}
-
 
 app.listen(config.api.port, () => {
     logger.info(`Server listening on http://localhost:${config.api.port}...`);
-    startDb();
 });
