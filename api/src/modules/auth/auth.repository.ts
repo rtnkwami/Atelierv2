36ff15df3bb6adc3b582b1784de14fcd5ddb33de
rtnkwami <nukukwami@outlook.com>
2025-10-17 23:28:42 +0000

    feat(api): add auth repo function to list permissions by default or filter

--- api/src/modules/auth/auth.repository.ts
+++ api/src/modules/auth/auth.repository.ts
@@ -7,6 +7,17 @@ type PermissionFilter = z.infer<typeof permissionRegex>;
 export type IAuthRepository = {
     permissions: {
         create: (permission: PermissionFilter) => Promise<Permissions>;
+        
+        list: (filter?: string, pagination?: {
+            page: number,
+            limit: number
+        }) => Promise<{
+            permissions: Permissions[],
+            page: number,
+            limit: number,
+            totalPages: number,
+            permissionsCount: number
+        }>;
     }
 }
 
@@ -20,6 +31,34 @@ export const createAuthRepository = (prisma: PrismaClient): IAuthRepository => (
             });
 
             return newPermission;
+        },
+
+        list: async (filter, pagination = { page: 1, limit: 20 }) => {            
+            const where = filter
+                ? {
+                    name: {
+                    contains: filter,
+                    mode: 'insensitive' as const,
+                    },
+                }
+                : {};
+
+            const [permissions, permissionsCount] = await prisma.$transaction([
+                prisma.permissions.findMany({
+                    where,
+                    skip: (pagination.page - 1) * pagination.limit,
+                    take: pagination.limit,
+                }),
+                prisma.permissions.count({ where }),
+            ]);
+
+            return {
+                permissions,
+                page: pagination.page,
+                limit: pagination.limit,
+                permissionsCount,
+                totalPages: Math.ceil(permissionsCount / pagination.limit),
+            };
         },
     }
 });
