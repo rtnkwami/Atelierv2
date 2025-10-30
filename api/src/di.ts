e5676aeea346dc7c9ac04f37438981c4600740f4
rtnkwami <nukukwami@outlook.com>
2025-10-30 13:24:07 +0000

    feat(api): rename di container file

--- api/src/di.ts
+++ api/src/di.ts
@@ -1,27 +1,34 @@
 import { createContainer, asFunction, asValue } from "awilix";
-import { prisma } from "@config/db.config";
-import { createUserRepository } from "modules/users/user.repository";
-import { createAuthService } from "@auth/auth.service";
-import { createAuthController } from "@auth/auth.controller";
-import { createUserService } from "modules/users/user.service";
-import { createUserController } from "modules/users/user.controller";
-import { createAuthRouter } from "@auth/auth.routes";
-import createAPI from "app";
-import { httpLogger } from "@config/logger.config";
+import { prisma } from "@config/db.config.ts";
+import { createUserRepository } from "./modules/users/user.repository.ts";
+import { createAuthService } from "@auth/auth.service.ts";
+import { createAuthController } from "@auth/auth.controller.ts";
+import { createUserService } from "modules/users/user.service.ts";
+import { createUserController } from "modules/users/user.controller.ts";
+import { createAuthRouter } from "@auth/auth.routes.ts";
+import createAPI from "app.ts";
+import logger, { httpLogger } from "@config/logger.config.ts";
+import createUserRouter from "modules/users/user.routes.ts";
 
 export default function createDiContainer () {
     const container = createContainer();
     
     container.register({
         db: asValue(prisma),
+        baseLogger: asValue(logger),
         httpLogger: asValue(httpLogger),
+        app: asFunction(createAPI).singleton(),
+
         authService: asFunction(createAuthService).scoped(),
-        authController: asFunction(createAuthController).scoped(),
-        authRouter: asFunction(createAuthRouter).singleton(),
-        userRepo: asFunction(createUserRepository).scoped(),
         userService: asFunction(createUserService).scoped(),
+        
         userController: asFunction(createUserController).scoped(),
-        app: asFunction(createAPI).singleton()
+        authController: asFunction(createAuthController).scoped(),
+        
+        userRepo: asFunction(createUserRepository).scoped(),
+        
+        authRouter: asFunction(createAuthRouter).singleton(),
+        userRouter: asFunction(createUserRouter).scoped(),
     });
 
     return container;
