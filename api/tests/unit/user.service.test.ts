acf9594ff4fa4249831b6103d30768ff3ed1da3a
rtnkwami <nukukwami@outlook.com>
2025-10-31 14:58:52 +0000

    feat(api): test if user service throws sync error on failure

--- api/tests/unit/user.service.test.ts
+++ api/tests/unit/user.service.test.ts
@@ -1,7 +1,7 @@
-import { beforeEach, afterAll, it, expect, describe, vi } from 'vitest';
+import { beforeEach, afterAll, it, expect, describe, vi, beforeAll, expectTypeOf } from 'vitest';
 import createDiContainer from '../../src/di.ts';
 import { mockDecodedIdToken } from '../mocks/firebase.mocks.ts';
-import { NotFoundError } from 'error.ts';
+import { NotFoundError, UserSyncError } from 'error.ts';
 import { Task } from 'true-myth/task';
 import { Users } from '../../prisma-client/client.ts';
 
@@ -18,27 +18,41 @@ describe('User Service ', () => {
     })
 
     describe('authenticated user sync to app db', () => {
+        const mockUser: Users = {
+            id: mockDecodedIdToken.uid,
+            name: mockDecodedIdToken.email,
+            email: mockDecodedIdToken.email,
+            avatar: mockDecodedIdToken.picture
+        }
+
         it('should create a user if they do not exist', async () => {            
-            const mockUser: Users = {
-                id: mockDecodedIdToken.uid,
-                name: mockDecodedIdToken.email,
-                email: mockDecodedIdToken.email,
-                avatar: mockDecodedIdToken.picture
-            }
-            
             const getUserTaskSpy = vi.spyOn(userRepo, 'getUser')
-                .mockReturnValue(Task.reject(new NotFoundError));
-            
-            const createUserTaskSpy = await vi.spyOn(userRepo, 'createUser').mockReturnValue(
+                .mockReturnValue(Task.reject(new NotFoundError)
+            );
+            const createUserTaskSpy = vi.spyOn(userRepo, 'createUser').mockReturnValue(
                 Task.resolve(mockUser)
             )
             const syncUserToDbTask = await userService.getOrCreateUser(mockDecodedIdToken);
 
             expect(syncUserToDbTask.isOk).toBe(true);
+            
             if (syncUserToDbTask.isOk) {
                 expect(getUserTaskSpy).toHaveBeenCalled()
                 expect(createUserTaskSpy).toHaveBeenCalledWith(mockUser);
                 expect(syncUserToDbTask.value).toMatchObject(mockUser)
+            }
+        });
+
+        it('should throw a user sync error on failure', async () => {
+            vi.spyOn(userRepo, 'getUser')
+                .mockReturnValue(Task.reject(new Error('Test Error'))
+            );
+            const syncUserToDbTask = await userService.getOrCreateUser(mockDecodedIdToken);
+
+            expect(syncUserToDbTask.isErr).toBe(true);
+            
+            if (syncUserToDbTask.isErr){
+                expect(syncUserToDbTask.error).toBeInstanceOf(UserSyncError);
             }
         })
     })
