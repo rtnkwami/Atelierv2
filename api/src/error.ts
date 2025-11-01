8e1f222295dbfed2d73bc544b46e097150d52f08
rtnkwami <nukukwami@outlook.com>
2025-11-01 21:50:29 +0000

    refactor(api): refactor user to local db sync function

--- api/src/error.ts
+++ api/src/error.ts
@@ -3,3 +3,5 @@ export class NotFoundError extends Error {};
 export class DatabaseError extends Error {};
 
 export class UserSyncError extends Error {};
+
+export class SellerUpgradeError extends Error {};
--- api/src/modules/users/user.service.ts
+++ api/src/modules/users/user.service.ts
@@ -1,14 +1,20 @@
 import { IUserRepository } from "./user.repository.ts"
 import { Users }from "@db-client/client.ts";
-import { NotFoundError, UserSyncError } from "error.ts";
+import { NotFoundError, SellerUpgradeError, UserSyncError } from "error.ts";
 import Task, { tryOrElse } from "true-myth/task";
 import { Logger } from "pino";
 import { DecodedIdToken } from "firebase-admin/auth";
+import { task } from "true-myth";
 
 
  export interface IUserService {
     getOrCreateUser: (userData: DecodedIdToken) => Task<Users, UserSyncError>;
-    // upgradeUserToSeller: (userId: string) => Task<Users, ServiceError>;
+    // upgradeUserToSeller: (userId: string) => Task<{
+    //         id: string,
+    //         roles: {
+    //             name: string
+    //         }[] 
+    //     }, SellerUpgradeError>;
 }
 
 type dependencies = {
@@ -21,38 +27,30 @@ export const createUserService = ({ userRepo, baseLogger }: dependencies): IUser
     const userServiceLogger = baseLogger.child({ module: 'users', layer: 'service' });
         
     return {
-        getOrCreateUser: (userData) =>
-            tryOrElse(
-                (reason) => {
+        getOrCreateUser: (userData) => {
+            const createUserDto: Users = {
+                id: userData.uid,
+                name: userData.email ?? '',
+                email: userData.email ?? '',
+                avatar: userData.picture ?? ''
+            }
+
+            return userRepo.getUser(createUserDto.id) // Ok(user)
+                .orElse(error => {
+                    if (error instanceof NotFoundError) {
+                        return userRepo.createUser(createUserDto)
+                            .map(user => {
+                                userServiceLogger.info(`User ${ user.id } created`);
+                                return user;
+                            })
+                    }
+                    return task.reject(error); // error other than NotFoundError
+                })
+                .mapRejected(reason => {
                     userServiceLogger.error({ error: reason }, 'Error syncing user');
-                    return new UserSyncError('Error syncing user', { cause: reason })
-                },
-                async () => {
-                    const createUserDto: Users = {
-                        id: userData.uid,
-                        name: userData.email ?? '',
-                        email: userData.email ?? '',
-                        avatar: userData.picture ?? ''
-                    }
-
-                    const getUserTask = await userRepo.getUser(createUserDto.id);
-                    
-                    if (getUserTask.isOk) {
-                        return getUserTask.value;
-                    };
-
-                    if (getUserTask.isErr && getUserTask.error instanceof NotFoundError) {
-                        const createUserTask = await userRepo.createUser(createUserDto);
-
-                        if (createUserTask.isErr) {
-                            throw createUserTask.error;
-                        }
-                        const user = createUserTask.value
-                        userServiceLogger.info(`User ${ user.id } created`);
-                        return user;
-                    }
-                    throw getUserTask.error;
-                }
-            ),
+                    return new UserSyncError('Error syncing user', { cause: reason });
+                })
         }
+        
     }
+}
