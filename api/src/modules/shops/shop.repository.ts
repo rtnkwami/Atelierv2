7676c6b5a01208edf856947cc5a8991504173a65
rtnkwami <nukukwami@outlook.com>
2025-11-07 15:07:02 +0000

    feat(api): add shop repo fn to get seller shop

--- api/src/modules/shops/shop.repository.ts
+++ api/src/modules/shops/shop.repository.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, Shops } from "@db-client/client.ts"
 import { getTxOrDbClient } from "async.context.ts";
 import { DatabaseError, NotFoundError } from "error.ts"
+import { Logger } from "pino";
 import Task, { tryOrElse } from 'true-myth/task'
 
 type CreateShopDto = {
@@ -11,52 +12,82 @@ type CreateShopDto = {
 export interface IShopRepository {
     createShop: (userId: string, shopData: CreateShopDto) => Task<Shops, DatabaseError>;
     getShop: (shopId: string) => Task<Shops, NotFoundError | DatabaseError>;
+    getSellerShop: (sellerId: string) => Task<Shops, NotFoundError | DatabaseError>;
 }
 
 
 type dependencies = {
     db: PrismaClient;
+    baseLogger: Logger;
 }
 
-export const createShopRepository = ({ db }: dependencies): IShopRepository => ({
-    createShop: (userId, shopData) =>
-        tryOrElse(
-            (reason) => new DatabaseError('Error creating shop', { cause: reason }),
-            async () => {
-                const client = getTxOrDbClient(db);
-                const shop = await client.shops.create({
-                    data: {
-                        name: shopData.name,
-                        description: shopData.description,
-                        user: {
-                            connect: { id: userId }
-                        }
-                    },
-                });
+export const createShopRepository = ({ db, baseLogger }: dependencies): IShopRepository => {
+    const shopRepoLogger = baseLogger.child({ module: "shops", layer: "repository"});
+ 
+    return {
+        createShop: (userId, shopData) =>
+            tryOrElse(
+                (reason) => new DatabaseError('Error creating shop', { cause: reason }),
+                async () => {
+                    const client = getTxOrDbClient(db);
+                    const shop = await client.shops.create({
+                        data: {
+                            name: shopData.name,
+                            description: shopData.description,
+                            user: {
+                                connect: { id: userId }
+                            }
+                        },
+                    });
 
-                return shop;
-            }
-        ),
-    getShop: (shopId) =>
-        tryOrElse(
-            (reason) => {
-                if (reason instanceof NotFoundError) {
-                    return reason;
+                    return shop;
                 }
-                return new DatabaseError(`Error getting shop "${ shopId }"`, { cause: reason });
-            },
-            async () => {
-                const client = getTxOrDbClient(db);
-                const shop = await client.shops.findUnique({
-                    where: {
-                        id: shopId
+            ),
+        getShop: (shopId) =>
+            tryOrElse(
+                (reason) => {
+                    if (reason instanceof NotFoundError) {
+                        return reason;
                     }
-                });
+                    return new DatabaseError(`Error getting shop "${ shopId }"`, { cause: reason });
+                },
+                async () => {
+                    const client = getTxOrDbClient(db);
+                    const shop = await client.shops.findUnique({
+                        where: {
+                            id: shopId
+                        }
+                    });
 
-                if (!shop) {
-                    throw new NotFoundError(`Shop "${ shopId }" does not exist`);
+                    if (!shop) {
+                        throw new NotFoundError(`Shop "${ shopId }" does not exist`);
+                    }
+                    return shop;
                 }
-                return shop;
-            }
-        )
-})
+            ),
+        getSellerShop: (sellerId) =>
+            tryOrElse(
+                (reason) => {
+                    if (reason instanceof NotFoundError) {
+                        return reason;
+                    }
+                    shopRepoLogger.error(reason, `Error getting shop for seller "${ sellerId } "`);
+                    return new DatabaseError(`Error getting shop for seller "${ sellerId }"`, { cause: reason })
+                },
+                async () => {
+                    const client = getTxOrDbClient(db);
+                    const shop = await client.shops.findUnique({
+                        where: {
+                            usersId: sellerId
+                        }
+                    });
+
+                    if (!shop) {
+                        throw new NotFoundError(`Shop does not exist for seller ${ sellerId }`);
+                    }
+                    return shop;
+                }
+            )
+    }
+
+}
